e06d0bb522ecc95322b088193522ba45e903795b
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2023-02-01 16:31:34 +0100

    added view grid and setup basic camera, model and light

--- src/webgl/Shape.ts
+++ src/webgl/Shape.ts
@@ -1,40 +1,66 @@
 /*
  * Shape.ts
  * ===========
- * Placeholder shape to demonstrate setup works. 
+ * Placeholder shape to demonstrate setup works.
  * Has capacity to import custom .glsl shader files
  */
 
 import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 import vertShader from "./glsl/torus.vs";
 import fragShader from "./glsl/torus.fs";
-import { randInt } from "Utils";
 
 export default class Shape {
-	mesh: THREE.Mesh;
-	timeU: THREE.IUniform;
+  object: THREE.Mesh;
+  timeU: THREE.IUniform;
 
-	constructor(parentScene: THREE.Scene) {
-		const geom = new THREE.TorusKnotGeometry(3, 1, 128, 32);
-		const mat = new THREE.RawShaderMaterial({
-			uniforms: {
-				time: {value: 0}
-			},
-			vertexShader: vertShader,
-			fragmentShader: fragShader
-		});
-		this.timeU = mat.uniforms.time;
-		this.mesh = new THREE.Mesh(geom, mat);
-		parentScene.add(this.mesh);
-	}
+  constructor(parentScene: THREE.Scene, camera: THREE.OrthographicCamera) {
+    const loader = new OBJLoader();
+    loader.load(
+      "./models/bunny.obj",
+      // called when resource is loaded
+      (loadedGroup) => {
+        console.log(loadedGroup.children[0]);
+        this.object = <THREE.Mesh>loadedGroup.children[0];
+        this.object.material = new THREE.MeshNormalMaterial();
+        this.object.geometry.center();
+        parentScene.add(this.object);
 
-	public update(secs: number): void {
-		this.timeU.value = secs;
-		this.mesh.rotation.set(
-			Math.sin(secs / 10) * 2 * Math.PI,
-			Math.cos(secs / 10) * 2 * Math.PI,
-			0
-		);
-	}
+        const box = new THREE.BoxHelper(this.object, 0xffff00);
+        parentScene.add(box);
+        this.setupCamera(camera);
+        console.log(`pos: ${this.object.position.toArray()}`);
+      },
+      // called when loading is in progresses
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log(`An error happened: ${error}`);
+      }
+    );
+  }
+
+  public getBoundingSphereRadius(): number {
+    let boundingBox = new THREE.BoxHelper(this.object);
+    return boundingBox.geometry.boundingSphere.radius;
+  }
+
+  public setupCamera(camera: THREE.OrthographicCamera) {
+    let aspect = window.innerWidth / window.innerHeight;
+    let frustumHeight = 2 * this.getBoundingSphereRadius();
+    camera.left = (frustumHeight * aspect) / -2;
+    camera.right = (frustumHeight * aspect) / 2;
+    camera.top = frustumHeight / 2;
+    camera.bottom = frustumHeight / -2;
+    camera.near = 0.1;
+    camera.far = 10;
+    camera.lookAt(0, 0, 0);
+    camera.position.z = 1;
+    camera.updateProjectionMatrix();
+  }
+
+  public update(secs: number): void {}
 }
--- src/webgl/View.ts
+++ src/webgl/View.ts
@@ -1,43 +1,66 @@
 /*
  * View.ts
  * ===========
- * Topmost Three.js class. 
+ * Topmost Three.js class.
  * Controls scene, cam, renderer, and objects in scene.
  */
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import GUI from "lil-gui";
 
 import Shape from "./Shape";
+import { Vector3 } from "three";
+import { SourceMapDevToolPlugin } from "webpack";
 
 export default class View {
-	private renderer: THREE.WebGLRenderer;
-	private scene: THREE.Scene;
-	private camera: THREE.PerspectiveCamera;
-	private torus: Shape;
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+  private camera: THREE.OrthographicCamera;
+  private model: Shape;
+  private gui: GUI;
+  private controls: OrbitControls;
+  private statsPanel: Stats;
 
-	constructor(canvasElem: HTMLCanvasElement) {
-		this.renderer = new THREE.WebGLRenderer({
-			canvas: canvasElem,
-			antialias: true,
-		});
-		this.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 100);
-		this.camera.position.z = 15;
-		this.scene = new THREE.Scene();
-		this.scene.background = new THREE.TextureLoader().load("./textures/bgnd.png");
-		this.torus = new Shape(this.scene);
+  constructor(canvasElem: HTMLCanvasElement) {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: canvasElem,
+      antialias: true,
+    });
 
-		// Set initial sizes
-		this.onWindowResize(window.innerWidth, window.innerHeight);
-	}
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.TextureLoader().load(
+      "./textures/bgnd.png"
+    );
 
-	public onWindowResize(vpW: number, vpH: number): void {
-		this.renderer.setSize(vpW, vpH);
-		this.camera.aspect = vpW / vpH;
-		this.camera.updateProjectionMatrix();
-	}
+    this.camera = new THREE.OrthographicCamera();
+    this.model = new Shape(this.scene, this.camera);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-	public update(secs: number): void {
-		this.torus.update(secs);
-		this.renderer.render(this.scene, this.camera);
-	}
+    this.statsPanel = Stats();
+    document.body.appendChild(this.statsPanel.dom);
+    this.gui = new GUI();
+
+    const axesHelper = new THREE.AxesHelper(1);
+    this.scene.add(axesHelper);
+
+    const light = new THREE.PointLight(0xff0000, 1, 100);
+    light.position.set(1, 1, 1);
+    this.scene.add(light);
+
+    // Set initial sizes
+    this.onWindowResize(window.innerWidth, window.innerHeight);
+  }
+
+  public onWindowResize(vpW: number, vpH: number): void {
+    this.renderer.setSize(vpW, vpH);
+    this.camera.updateProjectionMatrix();
+  }
+
+  public update(secs: number): void {
+    this.model.update(secs);
+    this.statsPanel.update();
+    this.renderer.render(this.scene, this.camera);
+  }
 }
