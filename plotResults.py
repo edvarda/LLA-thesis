de613253b6bd905fb04071f10608e9f924feac29
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2023-06-12 12:12:20 +0200

    plots and small david

--- plotResults.py
+++ plotResults.py
@@ -99,7 +99,7 @@ def createStrengthTestPlot(folder, results):
     )
 
 
-def createScaleSpaceTestPlot(folder, results):
+def createScaleSpaceTestPlot(results):
     fig, ax = plt.subplots(2, 2)
     resultGroups = getGroupedResults(results, "spatial")
     for (k, v), plot in zip(resultGroups.items(), ax.flat):
@@ -125,17 +125,73 @@ def load_object(filename):
             return pickle.load(f)
     except Exception as ex:
         print("Error during unpickling object (Possibly unsupported):", ex)
- 
+
+
+def linePlot(data):
+    # plt.rcParams['text.usetex'] = True
+    fig, ax = plt.subplots()
+
+    xValue = "strength" 
+    yValue = "diff"
+    legendTitle = "Scenario"
+    title = "Score improvement per scenario"
+    yLabel = "Congruence score improvement"
+    xLabel = "Enhancement strength"
+
+    david = data["david1"]
+    xValues = list({x.get(xValue) for x in david})
+    # print(xValues)
+
+    # xValues = ['Scale 1','Scale 2','Scale 3','Scale 4']
+
+
+    for scenario in data:
+        testCases = data[scenario]
+        xSeries = [x[xValue] for x in testCases]
+        ySeries = [x[yValue] for x in testCases]
+        ax.plot(xSeries, ySeries,label=scenario)
+    
+    x = np.arange(len(xValues))  # the label locations
+    width = 0.15  # the width of the bars
+    ax.set_ylabel(yLabel)
+    ax.set_xlabel(xLabel)
+    # ax.set_title(title)
+    ax.set_xticks(x + width, xValues)
+    ax.legend()
+    ax.grid()
+    ax.set_ylim(0, .6)
+    plt.show(block=True)
+    
+
+def formatDataForLinePlot(results):
+    
+    def testCaseFilter(case):
+      return case["range"].startswith("0.05") and case["spatial"].startswith("2.3") and case["sigma"] in ["1","2","3","4"]
+    def testCaseFilterS(case):
+      return case["spatial"].startswith("2.3") and case["sigma"].startswith("Coarse")
+    
+    data = dict()
+    for scenario in results:
+        if scenario == "map2": continue
+        series = filter(testCaseFilterS,results[scenario])
+        data[scenario] = list(series)
+    return data    
+        
+
 
 filename = sys.argv[1]
 dir = sys.argv[2]
 path = Path(dir)
 results = load_object(filename)
-createScaleSpaceTestPlot(results)
+print(type(results))
+data = formatDataForLinePlot(results)
+linePlot(data)
 
-saveFig(
-        (
-            path.parents[0]
-            / f"{str(path.name).removeprefix('ScaleSpaceTests_')}_scalePlot.png"
-        )
-    )
+# for scenario in results:
+#   createScaleSpaceTestPlot(results[scenario])
+#   saveFig(
+#           (
+#               path
+#               / f"{scenario}_scalePlot.png"
+#           )
+#       )
